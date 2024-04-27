ce55dbfd3ed6c1c0f2fe5866b142e3660ec1f6c3
Abraao Wendel <abraaowendel@protonmail.com>
2024-04-27 11:58:48 -0300

    Atualização de preço de 15 em 15 segundos.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
+const url =
+  "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,BRL";
+
 document.querySelector("span").innerText = new Date().getFullYear();
 
-
-
 async function fetchData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-
-      let dolar = document.getElementById("dolar");
-      let euro = document.getElementById("euro");
-      let real = document.getElementById("real");
-      
-      dolar.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'USD' }).format(data.USD);
-      euro.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'EUR' }).format(data.EUR);
-      real.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(data.BRL);
-      return;
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+
+    const dolar = document.getElementById("dolar");
+    const euro = document.getElementById("euro");
+    const real = document.getElementById("real");
+    const date = new Date();
+
+    dolar.innerText = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "USD",
+    }).format(data.USD);
+    euro.innerText = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "EUR",
+    }).format(data.EUR);
+    real.innerText = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(data.BRL);
+    document.querySelector(
+      "h4"
+    ).innerText = `Última atualização ás ${date.getHours()}h${date.getMinutes()}min${date.getSeconds()}s`;
+
+    return;
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
   }
-const url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,EUR,BRL";
+}
 
 fetchData(url);
 AOS.init();
 
-setInterval(() => fetchData(url), 5000)
+setInterval(() => fetchData(url), 15000);
